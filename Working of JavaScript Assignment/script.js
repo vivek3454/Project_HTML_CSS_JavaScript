d1a1d81bdcffbdf9f127e843e54a353729051365
Vivek <vivekparde2411@gmail.com>
2023-04-07 18:39:12 +0530

    working of javascript assignment

---
+++ Working of JavaScript Assignment/script.js
@@ -0,0 +1,49 @@
+// Q 1
+addNumbers(3,5);
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+
+// Q 2
+
+multiplyNumbers(3,5);
+const multiplyNumbers = function(num1, num2){
+    return num1 * num2;
+};
+
+
+// Q 3
+
+function sum(number1, number2) {
+    console.log(variable);
+    var variable;
+    return number1 + number2
+}
+
+sum()
+
+
+// Q 4
+
+{
+    // console.log(a);
+    // console.log(b);
+    console.log(c);
+
+    let a = 1;
+    const b = 2;
+    var c = 3;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+
+// Q 5
+
+{
+    let d;
+    console.log(d);
+}
